825acde5f4e95fbcaebce8d60ddd778a65a33088
truthixify <truthixify@gmail.com>
2024-09-24 01:24:35 +0100

    added generics for the data type in the cons list

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use List::{Cons, Nil};
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum List {
-  Cons(i32, Box<List>),
+pub enum List<T> {
+  Cons(T, Box<List<T>>),
   Nil,
 }
 
-impl List {
+impl<T: Clone> List<T> {
   pub fn new() -> Self {
     Nil
   }
@@ -18,7 +18,7 @@ impl List {
     }
   }
 
-  pub fn prepend(&mut self, element: i32) {
+  pub fn prepend(&mut self, element: T) {
     *self = Cons(element, Box::new(self.clone()));
   }
 
@@ -29,21 +29,21 @@ impl List {
     }
   }
 
-  pub fn head(&self) -> Option<&i32> {
+  pub fn head(&self) -> Option<&T> {
     match self {
         Cons(value, _) => Some(value),
         Nil => None,
     }
   }
 
-  pub fn tail(&self) -> Option<&List> {
+  pub fn tail(&self) -> Option<&List<T>> {
     match self {
       Cons(_, next) => Some(next),
       Nil => None,
     }
   }
 
-  pub fn pop(&mut self) -> Option<i32> {
+  pub fn pop(&mut self) -> Option<T> {
     match self.clone() {
         Cons(value, next) => {
           *self = *next;
@@ -53,14 +53,14 @@ impl List {
     }
   }
 
-  pub fn to_vec(&self) -> Vec<i32> {
+  pub fn to_vec(&self) -> Vec<T> {
     let mut vec_from_list = Vec::new();
     self.traverse_through_list(&mut vec_from_list);
 
     vec_from_list
   } 
 
-  pub fn reverse(&mut self) -> List {
+  pub fn reverse(&mut self) -> List<T> {
     let vec_from_list = self.to_vec();
     let mut new_list = List::new();
 
@@ -72,10 +72,10 @@ impl List {
   }
 
   // Helper function only used here to traverse through the list and store each value in an array
-  fn traverse_through_list(&self, value_vec: &mut Vec<i32>) -> Option<List> {
+  fn traverse_through_list(&self, value_vec: &mut Vec<T>) -> Option<List<T>> {
     match self {
         Cons(value, next) => {
-          value_vec.push(*value);
+          value_vec.push(value.clone());
           next.traverse_through_list(value_vec)
         },
         Nil => None
@@ -83,18 +83,18 @@ impl List {
   }
 }
 
-impl Iterator for List {
-  type Item = List;
-  fn next(&mut self) -> Option<Self::Item> {
-    match self.clone() {
-      Cons(_value, n) => {
-        *self = *n.clone();
-        Some(*n)
-      }
-      Nil => None
-    }
-  }
-}
+// impl Iterator for List<T> {
+//   type Item = List;
+//   fn next(&mut self) -> Option<Self::Item> {
+//     match self.clone() {
+//       Cons(_value, n) => {
+//         *self = *n.clone();
+//         Some(*n)
+//       }
+//       Nil => None
+//     }
+//   }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -102,14 +102,14 @@ mod tests {
 
   #[test]
   fn test_is_empty() {
-    let list = List::new();
+    let list: List<i32> = List::new();
 
     assert!(list.is_empty());
   }
 
   #[test]
   fn test_is_not_empty() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Cons(5, Box::new(Nil))))))))));
+    let list = Cons(String::from("a"), Box::new(Cons(String::from("b"), Box::new(Cons(String::from("c"), Box::new(Cons(String::from("d"), Box::new(Cons(String::from("e"), Box::new(Nil))))))))));
 
     assert!(!list.is_empty());
   }
@@ -133,9 +133,9 @@ mod tests {
 
   #[test]
   fn test_head() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Cons(5, Box::new(Nil))))))))));
+    let list = Cons(String::from("a"), Box::new(Cons(String::from("b"), Box::new(Cons(String::from("c"), Box::new(Cons(String::from("d"), Box::new(Cons(String::from("e"), Box::new(Nil))))))))));
 
-    assert_eq!(list.head(), Some(&1));
+    assert_eq!(list.head(), Some(&String::from("a")));
   }
 
   #[test]
@@ -160,11 +160,16 @@ mod tests {
 
   #[test]
   fn test_to_vec() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Cons(5, Box::new(Nil))))))))));
-    let expected_result = [1, 2, 3, 4, 5];
-    let result = list.to_vec();
+    let list_i32 = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Cons(4, Box::new(Cons(5, Box::new(Nil))))))))));
+    let expected_result_i32 = [1, 2, 3, 4, 5];
+    let result_i32 = list_i32.to_vec();
 
-    assert_eq!(result, expected_result);
+    let list_string = Cons(String::from("a"), Box::new(Cons(String::from("b"), Box::new(Cons(String::from("c"), Box::new(Cons(String::from("d"), Box::new(Cons(String::from("e"), Box::new(Nil))))))))));
+    let expected_result_string = [String::from("a"), String::from("b"), String::from("c"), String::from("d"), String::from("e")];
+    let result_string = list_string.to_vec();
+
+    assert_eq!(result_i32, expected_result_i32);
+    assert_eq!(result_string, expected_result_string);
   }
 
   #[test]
